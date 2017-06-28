11d88d620acdb9cd7291f95f9a624a43d92fa517
yuhuashi <906846877@qq.com>
2017-06-28 09:32:57 +0800

    path:'/home',  斜杠前不加 .

--- src/router.js
+++ src/router.js
@@ -1,33 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
 import homePage from './views/home.vue'
 import aboutPage from './views/about.vue'
 import loginPage from './views/login.vue'
 import registerPage from './views/register.vue'
 
 Vue.use(Router)
-
 export default new Router({
     routes: [
         {
             path: '/home',
-            // name: 'home',
+            name: 'home',
             component: homePage
         },
         {
             path: '/about',
-            // name: 'about',
+            name: 'about',
             component: aboutPage
         },
         {
             path: '/register',
-            // name: 'register',
+            name: 'register',
             component: registerPage
         },
          {
             path: '/login',
-            // name: 'login',
+            name: 'login',
             component: loginPage
         },
         {
