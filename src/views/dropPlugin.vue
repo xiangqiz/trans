74482ba0338a1185c3aeceb48d445f6396af73f5
yuhuashi <906846877@qq.com>
2017-08-18 18:09:25 +0800

    下拉框多选

--- src/views/dropPlugin.vue
+++ src/views/dropPlugin.vue
@@ -1,11 +1,12 @@
 <template>
 	<div>
-		<div id="showDrop">
-			<input class="showDropDown" @click="toggleDrop($event)" 
-				:value="dropResult[0]?dropResult[0][dropDownOpt.showField]:'' " 
-				spellcheck="false" />
-			<dropDownComponent :optObj="dropDownOpt" @chooseItem="getDropItems"></dropDownComponent>
-		</div>
+		<input class="dropOne" @click.stop="toggleDrop(clickDrop,$event)"
+			:value="123"
+			spellcheck="false" />
+		<input class="dropMulti" @click.stop="toggleDrop(multiDrop,$event)"
+			:value="123"
+			spellcheck="false" />
+		<dropDownComponent :optObj="dropDownOpt" @chooseItem="getDropItems"></dropDownComponent>
 	</div>
 </template>
 <script>
@@ -13,8 +14,9 @@
 		name:'dropPlugin',
 		data(){
 			return {
-				dropResult:[],
-				dropDownOpt:{
+				dropResult:{},
+				clickDrop:{
+					name:'borderRadius',
 					isShow:false,//初始化 是否显示下拉框
 					dropItems:[
 						{type:'0',text:'0px'},
@@ -28,23 +30,77 @@
 					realField:'type',
 					defaultItem:'',//选中项
 					isSearch:true,//是否要搜索功能
-					isMultiple:true,//是否可以多选
+				},
+				multiDrop:{
+					name:'multi',
+					isShow:false,//初始化 是否显示下拉框
+					dropItems:[
+						{type:'上海',text:'上海'},
+						{type:'北京',text:'北京'},
+						{type:'河北',text:'河北'},
+						{type:'河南',text:'河南'},
+						{type:'南阳',text:'南阳'},
+						{type:'洛阳',text:'洛阳'},
+					],
+					showField:'text',
+					realField:'type',
+					defaultItem:[],//选中项
+					isSearch:true,//是否要搜索功能
+					isMultiple:true,
+					multiLength:3,
+				},
+				curDrop:{},
+			}
+		},
+		mounted(){
+			let _this=this;
+			window.onclick=function(){
+				if(_this.dropDownOpt.isShow){
+					_this.dropDownOpt.isShow=false;
 				}
 			}
 		},
+		watch:{
+
+		},
+		computed:{
+			dropDownOpt(){
+				let _obj={};
+				if(this.curDrop.name==='borderRadius'){
+					_obj=this.clickDrop;
+				}else if(this.curDrop.name==='multi'){
+					_obj=this.multiDrop;
+				}
+				return _obj;
+			},
+		},
 		methods:{
-			toggleDrop(e){
-				this.dropDownOpt.isShow=!this.dropDownOpt.isShow;
-				this.dropDownOpt.el=e.target;
-				if(this.dropResult.length>0){
-					this.dropDownOpt.defaultItem=this.dropResult[0][this.dropDownOpt.realField];
+			toggleDrop(dropOpt,e){
+				this.curDrop=dropOpt;
+				dropOpt.isShow=!dropOpt.isShow;
+				dropOpt.el=e.target;
+				if(dropOpt.isMultiple){
+					if(this.dropResult[dropOpt.name] && this.dropResult[dropOpt.name].length>0){
+						dropOpt.defaultItem=[];
+						dropOpt.defaultItem=this.dropResult[dropOpt.name];
+					}
+				}else{
+					if(this.dropResult[dropOpt.name]){
+						dropOpt.defaultItem=this.dropResult[dropOpt.name][this.dropDownOpt.realField];
+					}
 				}
 			},
 			getDropItems(msg){
 				console.log(msg);
-				this.dropResult=[];
-				this.dropResult.push(msg);
-				// this.dropDownOpt.isShow=false;
+				if(this.curDrop.name==='borderRadius'){
+					this.dropResult['borderRadius']=msg;
+					this.dropDownOpt.isShow=false;
+				}else if(this.curDrop.name==='multi'){
+					this.dropResult['multi']=[];
+					for(let item of msg){
+						this.dropResult['multi'].push(item[this.dropDownOpt.realField]);
+					}
+				}
 			}
 		}
 	}
